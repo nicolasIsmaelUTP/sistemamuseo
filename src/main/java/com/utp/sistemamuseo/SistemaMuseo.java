1df3c647616efb1dfba2929713483d4531a7151a
Nicolas Alayo <u21221601@utp.edu.pe>
2023-06-01 10:11:09 -0500

    Primer commit

---
+++ src/main/java/com/utp/sistemamuseo/SistemaMuseo.java
@@ -0,0 +1,12 @@
+package com.utp.sistemamuseo;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class SistemaMuseo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
